e0b683a140fe2b76f8e537ed471de5d8b007424e
Mahendra Chouhan <mchouhanofficial@gmail.com>
2024-06-07 17:18:21 +0530

    Updated About me section, and added contact links.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Container, Box, Grid, Avatar, Link, Stack } from '@mui/material';
-import { Email } from '@mui/icons-material';
+import { Email, LinkedIn, RssFeed } from '@mui/icons-material';
 
 function App() {
   return (
     <div>
-      <AppBar position="static">
+      <Container>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Mahendra's Portfolio
@@ -18,38 +18,42 @@ function App() {
             Contact Me
           </Button>
         </Toolbar>
-      </AppBar>
-      <Container>
+
         <Box sx={{ my: 4 }}>
           <Grid container spacing={2} alignItems="center">
-            <Grid item xs={12} md={4}>
+            <Grid item xs={8} md={8}>
               <Avatar
                 alt="Mahendra Chouhan"
                 src="/logo.jpg"
-                sx={{ width: 200, height: 200, mx: 'auto' }}
+                sx={{ width: 400, height: 400, mx: 'auto' }}
               />
             </Grid>
             <Grid item xs={12} md={8}>
-              <Typography variant="h2" component="h2" gutterBottom>
+              <Typography variant="h6" component="h1" gutterBottom>
                 Hello, I'm Mahendra Chouhan
               </Typography>
-              <Typography variant="body1" paragraph gutterBottom>
-                Welcome to my portfolio! My name is Mahendra Chouhan, I am a fullstack developer by profession and engineer by heart.
-                I love building scalable web and mobile applications, and tinker with various systems in my free time.
-                I built my first application, which was a snake game when I was in school, and ever since then I am in love with programming.
-                If you want to get in touch, reach out to me at mchouhanofficial@gmail.com.
-
-                My intention behind creating this website is to share what I know and create a community of learners, who can keep sharing and improving each other.
-                That means i will be regularly posting some content here, be it something that I read or my own thoughts, with the intention to become a better software engineer.
-                Lets Grow together!!
+              <Typography variant="body1" paragraph>
+                Welcome to my portfolio! My name is Mahendra Chouhan. I am a full-stack developer by profession and an engineer at heart. I am passionate about building scalable web and mobile applications and enjoy tinkering with various systems in my free time.
               </Typography>
-              <Typography variant="h4" component="h4" paragraph>
-                You can find my content on                
+              <Typography variant="body1" paragraph>
+                My journey in programming began when I built my first application, a simple snake game, while I was still in school. Ever since then, I have been deeply in love with coding and software development. Currently, I am working as a software engineer at Google, where I continue to hone my skills and take on exciting challenges.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                This website serves as a platform for me to share my knowledge and foster a community of learners. Here, you will find regular posts about various topics, ranging from interesting reads to my personal thoughts and experiences in software engineering. My goal is to learn and grow together with you, as we strive to become better engineers and innovators.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                If you would like to get in touch, feel free to reach out to me at mchouhanofficial@gmail.com. Let's grow and improve together!
               </Typography>
               <Stack spacing={2} direction="row">
-                <Button variant="outlined"> Medium </Button>
-                <Button variant="outlined"> Enjoy Algorithms</Button>
-                <Button variant="outlined"> LinkedIn </Button>
+                <Button
+                  variant="outlined" color="inherit"
+                  startIcon={<RssFeed />}
+                  href="https://medium.com/@mchouhanofficial"> Medium </Button>
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  startIcon={<LinkedIn />}
+                  href="https://www.linkedin.com/in/mchouhanofficial/"> LinkedIn </Button>
               </Stack>
             </Grid>
           </Grid>
