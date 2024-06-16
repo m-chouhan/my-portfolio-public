2a2105dd56d81d0e4fe1254b348ae0490e265ea1
Mahendra Chouhan <mchouhanofficial@gmail.com>
2024-06-16 18:32:20 +0530

    minor fixes to home & writings page.

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -1,8 +1,17 @@
 // Writings.js
 import React from 'react';
 
+import { Typography, Link, Button, Container, Box, Grid, Avatar, Stack, Tabs, Tab } from '@mui/material';
+
 const Writings = () => {
-    return <div>Writings Page</div>;
+    return (
+        <>
+            <Typography variant="body1" paragraph>
+                I also write about software development in my free time,
+                You can check out my blogs on medium and enjoyalgorithms.
+            </Typography>
+        </>
+    );
 };
 
 export default Writings;
